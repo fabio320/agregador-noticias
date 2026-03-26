e7f9d43e5f26ff8ba60892a86fc4311de1029718
fabio320 <fabiofran15467@gmail.com>
2026-03-26 14:37:37 -0400

    Update V3__create_view.sql

--- scripts/V3__create_view.sql
+++ scripts/V3__create_view.sql
@@ -1,1 +1,11 @@
+-- Criação de uma view
 
+CREATE OR REPLACE VIEW view_noticias_comentarios AS
+SELECT 
+    n.titulo,
+    u.nome AS usuario,
+    c.conteudo,
+    c.data
+FROM comentarios c
+JOIN usuarios u ON c.id_usuario = u.id_usuario
+JOIN noticias n ON c.id_noticia = n.id_noticia;
