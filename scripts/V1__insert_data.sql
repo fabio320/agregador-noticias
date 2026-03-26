96e58a809178b413bde59ce80c415dac3acb4bb1
fabio320 <fabiofran15467@gmail.com>
2026-03-26 14:36:11 -0400

    Update V1__insert_data.sql

--- scripts/V1__insert_data.sql
+++ scripts/V1__insert_data.sql
@@ -1,1 +1,17 @@
+-- Inserção de dados
 
+INSERT INTO usuarios (nome, email, senha) VALUES
+('João', 'joao@email.com', '123'),
+('Maria', 'maria@email.com', '456');
+
+INSERT INTO noticias (titulo, link, fonte, data_publicacao) VALUES
+('Nova tecnologia lançada', 'https://site.com/noticia1', 'TechNews', '2026-03-25'),
+('Economia em alta', 'https://site.com/noticia2', 'FinanceNews', '2026-03-24');
+
+INSERT INTO comentarios (conteudo, id_usuario, id_noticia) VALUES
+('Muito interessante!', 1, 1),
+('Não gostei muito.', 2, 2);
+
+INSERT INTO reacoes (tipo, id_usuario, id_noticia) VALUES
+('relevante', 1, 1),
+('sem_interesse', 2, 2);
