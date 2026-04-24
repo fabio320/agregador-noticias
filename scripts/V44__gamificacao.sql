fe7ff7f1e9a79cc612df54729ffb1f3cdb82adff
fabio320 <fabiofran15467@gmail.com>
2026-04-23 23:04:24 -0400

    Update V44__gamificacao.sql

--- scripts/V44__gamificacao.sql
+++ scripts/V44__gamificacao.sql
@@ -1,1 +1,13 @@
+CREATE TABLE IF NOT EXISTS niveis (
+    id_nivel SERIAL PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    pontuacao_minima INT NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS pontos (
+    id_usuario INT PRIMARY KEY,
+    pontuacao INT DEFAULT 0,
+    id_nivel INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario) ON DELETE CASCADE,
+    FOREIGN KEY (id_nivel) REFERENCES niveis(id_nivel)
+);
