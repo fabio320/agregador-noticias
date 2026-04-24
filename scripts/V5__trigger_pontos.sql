0778c20d9ab2eb370ff33d3945ac63f15b6211e5
fabio320 <fabiofran15467@gmail.com>
2026-04-23 23:04:38 -0400

    Update V5__trigger_pontos.sql

--- scripts/V5__trigger_pontos.sql
+++ scripts/V5__trigger_pontos.sql
@@ -1,1 +1,15 @@
+CREATE OR REPLACE FUNCTION adicionar_pontos_comentario()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE pontos
+    SET pontuacao = pontuacao + 3
+    WHERE id_usuario = NEW.id_usuario;
 
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_comentario
+AFTER INSERT ON comentarios
+FOR EACH ROW
+EXECUTE FUNCTION adicionar_pontos_comentario();
